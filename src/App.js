93b9987f931d4ffbdd7688a71e95acd208c640e7
Alok kumar <devopsalok@gmail.com>
2020-05-08 02:03:29 +0530

    file changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to 82 Technology
+          82 Technology 
         </p>
           Learn React
       </header>
