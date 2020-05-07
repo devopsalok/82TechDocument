ddfcea8251bcc1f9a284f677effc3ae37b22a94e
Alok kumar <devopsalok@gmail.com>
2020-05-08 01:49:11 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to 82 Technology
+        </p>
+          Learn React
+      </header>
+    </div>
+  );
+}
+
+export default App;
